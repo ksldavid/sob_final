3a95f7dd3bf0ce331e7b1ed2be759a0740e64599
ksldavid <davidmwenebatu@gmail.com>
2022-12-04 19:45:29 +0400

    verry  easy to fixe, minor errors, commas, parantheses, etc

---
+++ sob2.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password? : ") #added a (")
+if greeting in ("Arrr!"): #changed [ with )
+	print("Go away, pirate.")
+else:  #changed elif with else
+	print("Greetings, hater of pirates!") #added indentation
